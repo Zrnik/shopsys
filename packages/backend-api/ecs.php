75824371e2b1fb02b42d1b9aad9a999f144d0ed0
Robin Dvořák <dvorak@uxf.cz>
2022-05-16 13:48:58 +0200

    [shopsys] Refactor ECS config to new syntax

--- packages/backend-api/ecs.php
+++ packages/backend-api/ecs.php
@@ -6,34 +6,33 @@ use ObjectCalisthenics\Sniffs\Files\FunctionLengthSniff;
 use Shopsys\CodingStandards\CsFixer\ForbiddenPrivateVisibilityFixer;
 use Shopsys\CodingStandards\Sniffs\ForceLateStaticBindingForProtectedConstantsSniff;
 use Shopsys\CodingStandards\Sniffs\ObjectIsCreatedByFactorySniff;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 /**
- * @param \Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator $containerConfigurator
+ * @param Symplify\EasyCodingStandard\Config\ECSConfig $ecsConfig
  */
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $services = $containerConfigurator->services();
-    $parameters = $containerConfigurator->parameters();
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->rule(ForceLateStaticBindingForProtectedConstantsSniff::class);
 
-    $services->set(ForceLateStaticBindingForProtectedConstantsSniff::class);
+    /*
+     * this package is meant to be extensible using class inheritance,
+     * so we want to avoid private visibilities in the model namespace
+     */
+    $ecsConfig->ruleWithConfiguration(ForbiddenPrivateVisibilityFixer::class, [
+        'analyzed_namespaces' => [
+            'Shopsys\BackendApiBundle',
+        ],
+    ]);
 
-    // this package is meant to be extensible using class inheritance, so we want to avoid private visibilities in the model namespace
-    $services->set('forbidden_private_visibility_fixer.backend_api', ForbiddenPrivateVisibilityFixer::class)
-        ->call('configure', [['analyzed_namespaces' => ['Shopsys\BackendApiBundle']]]);
+    $ecsConfig->skip([
+        ObjectIsCreatedByFactorySniff::class => [
+            __DIR__ . '/tests/*',
+        ],
+        FunctionLengthSniff::class => [
+            __DIR__ . '/src/Controller/**/*Validator.php',
+            __DIR__ . '/install/tests/App/Smoke/BackendApiCreateProductTest.php',
+        ],
+    ]);
 
-    $parameters->set(
-        Option::SKIP,
-        [
-            ObjectIsCreatedByFactorySniff::class => [
-                __DIR__ . '/tests/*',
-            ],
-            FunctionLengthSniff::class => [
-                __DIR__ . '/src/Controller/**/*Validator.php',
-                __DIR__ . '/install/tests/App/Smoke/BackendApiCreateProductTest.php',
-            ],
-        ]
-    );
-
-    $containerConfigurator->import(__DIR__ . '/vendor/shopsys/coding-standards/ecs.php', null, true);
+    $ecsConfig->import(__DIR__ . '/vendor/shopsys/coding-standards/ecs.php', null, true);
 };
--- packages/frontend-api/ecs.php
+++ packages/frontend-api/ecs.php
@@ -6,34 +6,33 @@ use ObjectCalisthenics\Sniffs\Files\FunctionLengthSniff;
 use Shopsys\CodingStandards\CsFixer\ForbiddenPrivateVisibilityFixer;
 use Shopsys\CodingStandards\Sniffs\ForceLateStaticBindingForProtectedConstantsSniff;
 use Shopsys\CodingStandards\Sniffs\ObjectIsCreatedByFactorySniff;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 /**
- * @param \Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator $containerConfigurator
+ * @param Symplify\EasyCodingStandard\Config\ECSConfig $ecsConfig
  */
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $services = $containerConfigurator->services();
-    $parameters = $containerConfigurator->parameters();
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->rule(ForceLateStaticBindingForProtectedConstantsSniff::class);
 
-    $services->set(ForceLateStaticBindingForProtectedConstantsSniff::class);
+    /*
+     * this package is meant to be extensible using class inheritance,
+     * so we want to avoid private visibilities in the model namespace
+     */
+    $ecsConfig->ruleWithConfiguration(ForbiddenPrivateVisibilityFixer::class, [
+        'analyzed_namespaces' => [
+            'Shopsys\FrontendApiBundle',
+        ],
+    ]);
 
-    // this package is meant to be extensible using class inheritance, so we want to avoid private visibilities in the model namespace
-    $services->set('forbidden_private_visibility_fixer.frontend_api', ForbiddenPrivateVisibilityFixer::class)
-        ->call('configure', [['analyzed_namespaces' => ['Shopsys\FrontendApiBundle']]]);
+    $ecsConfig->skip([
+        ObjectIsCreatedByFactorySniff::class => [
+            __DIR__ . '/tests/*',
+        ],
+        FunctionLengthSniff::class => [
+            __DIR__ . '/tests/FrontendApiBundle/Functional/Article/GetArticlesTest.php',
+            __DIR__ . '/src/Model/Resolver/Products/ProductResolverMap.php',
+        ],
+    ]);
 
-    $parameters->set(
-        Option::SKIP,
-        [
-            ObjectIsCreatedByFactorySniff::class => [
-                __DIR__ . '/tests/*',
-            ],
-            FunctionLengthSniff::class => [
-                __DIR__ . '/tests/FrontendApiBundle/Functional/Article/GetArticlesTest.php',
-                __DIR__ . '/src/Model/Resolver/Products/ProductResolverMap.php',
-            ],
-        ]
-    );
-
-    $containerConfigurator->import(__DIR__ . '/vendor/shopsys/coding-standards/ecs.php', null, true);
+    $ecsConfig->import(__DIR__ . '/vendor/shopsys/coding-standards/ecs.php', null, true);
 };
